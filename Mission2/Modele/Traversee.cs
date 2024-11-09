90b109e84afe2b3f14a74da02befd93658ec2107
florentlt <florent.louet11@gmail.com>
2024-11-09 22:31:20 +0100

    Affichage des liaisons

---
+++ Mission2/Modele/Traversee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Mission2.Modele
+{
+    public class Traversee
+    {
+        private string idTraversee;
+        private string idBateau;
+        private Liaison liaison;
+        private DateTime dateTraversee;
+        private TimeSpan heure;
+
+        public Traversee(string idTraversee, string idBateau, Liaison liaison, DateTime dateTraversee, TimeSpan heure)
+        {
+            this.idTraversee = idTraversee;
+            this.idBateau = idBateau;
+            this.liaison = liaison;
+            this.dateTraversee = dateTraversee;
+            this.heure = heure;
+        }
+
+        public string IdTraversee
+        {
+            get => idTraversee;
+        }
+
+        public string IdBateau
+        {
+            get => idBateau;
+        }
+
+        public Liaison Liaison
+        {
+            get => liaison;
+        }
+
+        public DateTime DateTraversee
+        {
+            get => dateTraversee;
+        }
+
+        public TimeSpan Heure
+        {
+            get => heure;
+        }
+    }
+}
---
+++ Mission2/Vue/Gestion.cs
@@ -0,0 +1,48 @@
+﻿using Mission2.Controleur;
+using Mission2.Modele;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mission2
+{
+    public partial class Gestion : Form
+    {
+        Mgr monManager;
+        List<Liaison> lstLiaison = new List<Liaison>();
+        public Gestion()
+        {
+            InitializeComponent();
+            monManager = new Mgr();
+        }
+
+        public void afficherLiaison()
+        {
+            try
+            {
+                lbLiaison.DataSource = null;
+                lbLiaison.DataSource = lstLiaison;
+                lbLiaison.DisplayMember = "Description";
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Gestion_Load(object sender, EventArgs e)
+        {
+            lstLiaison = monManager.chargementLiaisonBD();
+
+
+            afficherLiaison();
+        }
+    }
+}
